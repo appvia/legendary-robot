7fc2618b5a83ce62b9ecab3115ed7a7c8a2e932a
Tim Stahl <tim.stahl@appvia.io>
2022-05-11 12:50:57 -0400

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "legendary-robot-resources"
+  location = "eastus"
+
+  tags = {
+    Environment = "Tim Stahl - Testing GitHub Actions w/ TF"
+    Team        = "Sales"
+  }
+}
